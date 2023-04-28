d7ad4f9cc8a1744b9fb38e002bed98342853362c
kumarikanak <kumarikanak@users.noreply.github.com>
2023-04-28 19:40:47 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,2 @@
+ERROR_CODES = {"E416":{"code": "E416","status": "failure","description": "Too many pages in the pdf", "message": "The uploaded pdf exceeds max page limit, please upload a pdf with max 3 pages." },
+               "E417":{"code": "E417","status": "failure","description": "Not able to count pages in pdf", "message": "Please check photo_url request data." }}
